b406403d6ff587b96af74582010c2722ea1d7eff
Mostafa <mostafa.socialx@gmail.com>
2026-09-14 17:36:23 +0300

    feat: add models
    Implemented the models for Assignment 2 and addressed the review comments.

---
+++ model/Cake.model.ts
@@ -0,0 +1,110 @@
+import { IItem, ItemCategory } from "./IItem";
+
+export class Cake implements IItem {
+  private type: string;
+  private flavor: string;
+  private filling: string;
+  private size: number;
+  private layers: number;
+  private frostingType: string;
+  private frostingFlavor: string;
+  private decorationType: string;
+  private decorationColor: string;
+  private customMessage: string;
+  private shape: string;
+  private allergies: string;
+  private specialIngredients: string;
+  private packagingType: string;
+
+  constructor(
+    type: string,
+    flavor: string,
+    filling: string,
+    size: number,
+    layers: number,
+    frostingType: string,
+    frostingFlavor: string,
+    decorationType: string,
+    decorationColor: string,
+    customMessage: string,
+    shape: string,
+    allergies: string,
+    specialIngredients: string,
+    packagingType: string,
+  ) {
+    this.type = type;
+    this.flavor = flavor;
+    this.filling = filling;
+    this.size = size;
+    this.layers = layers;
+    this.frostingType = frostingType;
+    this.frostingFlavor = frostingFlavor;
+    this.decorationType = decorationType;
+    this.decorationColor = decorationColor;
+    this.customMessage = customMessage;
+    this.shape = shape;
+    this.allergies = allergies;
+    this.specialIngredients = specialIngredients;
+    this.packagingType = packagingType;
+  }
+
+  public getCategory(): ItemCategory {
+    return ItemCategory.CAKE;
+  }
+
+  public getType(): string {
+    return this.type;
+  }
+
+  public getFlavor(): string {
+    return this.flavor;
+  }
+
+  public getFilling(): string {
+    return this.filling;
+  }
+
+  public getSize(): number {
+    return this.size;
+  }
+
+  public getLayers(): number {
+    return this.layers;
+  }
+
+  public getFrostingType(): string {
+    return this.frostingType;
+  }
+
+  public getFrostingFlavor(): string {
+    return this.frostingFlavor;
+  }
+
+  public getDecorationType(): string {
+    return this.decorationType;
+  }
+
+  public getDecorationColor(): string {
+    return this.decorationColor;
+  }
+
+  public getCustomMessage(): string {
+    return this.customMessage;
+  }
+
+  public getShape(): string {
+    return this.shape;
+  }
+
+  public getAllergies(): string {
+    return this.allergies;
+  }
+
+  public getSpecialIngredients(): string {
+    return this.specialIngredients;
+  }
+
+  public getPackagingType(): string {
+    return this.packagingType;
+  }
+}
---
+++ model/Toy.model.ts
@@ -0,0 +1,54 @@
+import { IItem, ItemCategory } from "./IItem";
+
+export class Toy implements IItem {
+  private type: string;
+  private ageGroup: string;
+  private brand: string;
+  private material: string;
+  private batteryRequired: boolean;
+  private educational: boolean;
+
+  constructor(
+    type: string,
+    ageGroup: string,
+    brand: string,
+    material: string,
+    batteryRequired: boolean,
+    educational: boolean,
+  ) {
+    this.type = type;
+    this.ageGroup = ageGroup;
+    this.brand = brand;
+    this.material = material;
+    this.batteryRequired = batteryRequired;
+    this.educational = educational;
+  }
+
+  public getCategory(): ItemCategory {
+    return ItemCategory.TOY;
+  }
+
+  public getType(): string {
+    return this.type;
+  }
+
+  public getAgeGroup(): string {
+    return this.ageGroup;
+  }
+
+  public getBrand(): string {
+    return this.brand;
+  }
+
+  public getMaterial(): string {
+    return this.material;
+  }
+
+  public getBatteryRequired(): boolean {
+    return this.batteryRequired;
+  }
+
+  public getEducational(): boolean {
+    return this.educational;
+  }
+}
